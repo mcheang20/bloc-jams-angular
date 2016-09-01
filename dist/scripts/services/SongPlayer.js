66091b77a4ea3e0b49a15ae0a6c7d7d97cf474e8
mattcheang <cheangmatt@gmail.com>
2016-09-01 16:16:55 -0700

    assignment 7 complete

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -28,12 +28,16 @@
  
     currentSong = song;
  };
+
+          var playSong = function(song) {
+            currentBuzzObject.play();
+            currentSong.playing = true;
+        }
          
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
             setSong(song);
-            currentBuzzObject.play();  
-            song.playing = true;
+            playSong(song);
         } else if (currentSong === song) {
             if (currentBuzzObject.isPaused()) {
              currentBuzzObject.play();
