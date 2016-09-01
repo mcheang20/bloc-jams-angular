8294c4488426473c7153af697dac932b1bf17365
mattcheang <cheangmatt@gmail.com>
2016-09-01 14:41:42 -0700

    assignmen 6 complete

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -1,3 +1,7 @@
+(function() {
+     function Fixtures() {
+         var Fixtures = {};
+         
 var albumPicasso = {
      title: 'The Colors',
      artist: 'Pablo Picasso',
@@ -27,3 +31,23 @@ var albumMarconi = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+         
+        Fixtures.getAlbum = function() {
+         return albumPicasso;
+     };
+         
+        Fixtures.getCollection = function(numberOfAlbums) {
+            var albums = [];
+            for(var i = 0; i < numberOfAlbums; i++) {
+                albums.push(angular.copy(albumPicasso));
+            }
+            return albums;
+        };
+
+         return Fixtures;
+     }
+ 
+     angular
+         .module('blocJams')
+         .factory('Fixtures', Fixtures);
+ })();
