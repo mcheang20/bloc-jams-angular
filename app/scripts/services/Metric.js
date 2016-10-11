0c44d52b401b2976b548adb566b99c3eaaa93005
mattcheang <cheangmatt@gmail.com>
2016-10-10 18:13:41 -0700

    register album view metric add

--- app/scripts/services/Metric.js
+++ app/scripts/services/Metric.js
@@ -1,6 +1,8 @@
 (function() {
   function Metric($rootScope) {
     $rootScope.songPlays = [];
+    $rootScope.registerAlbum = [];
+    
     var date = new Date();
     var formatDate = moment(date);
     var newDate =  formatDate.format('MMM Do YY');
@@ -9,16 +11,29 @@
       // Function that records a metric object by pushing it to the $rootScope array
       registerSongPlay: function(song) {
         // Add time to event register
-        
         song['playedAt'] = newDate;
         $rootScope.songPlays.push(song);
       },
-      listSongsPlayed: function() {
-        var songs = [];
-        angular.forEach($rootScope.songPlays, function(song) {
-            songs.push(song.title);
-        });
-        return songs;
+        
+       listSongsPlayed: function() {
+            var songs = [];
+            angular.forEach($rootScope.songPlays, function(song) {
+                songs.push(song.title);
+            });
+            return songs;
+      },
+        
+      registerAlbumView: function(album) {
+        album['title'] = album.title;
+        $rootScope.registerAlbum.push(album);
+      },
+        
+      listAlbumViews: function() {
+          var albums = [];
+          angular.forEach($rootScope.registerAlbum, function(album) {
+            albums.push(album.title);      
+          });
+          return albums;
       }
     };
   }
